a9717fce5cdd6b9d587a976a55e61228e89f8870
Will Ondrik <wondrik@my.bcit.ca>
2024-12-17 06:36:01 -0800

    Consolidated GoogleService/Dtos into AnalyticsService/Dtos.

--- src/api/services/analytics/AnalyticsDtos.ts
+++ src/api/services/analytics/AnalyticsDtos.ts
@@ -16,3 +16,34 @@ export interface GaRequest {
     comparisonPeriod: GaTimePeriod;
     timeframe: string;
 }
+
+/**
+ * GaAccountsContextType represents the shape of the context that is used to store the Google Analytics accounts and properties.
+ */
+export interface GaAccountsContextType {
+    accounts: GaAccount[];
+    defaultAccount: GaAccount | null;
+    defaultProperty: GaProperty | null;
+    isLoading: boolean;
+}
+
+/**
+ * GaAccount represents the shape of a Google Analytics account.
+ */
+export interface GaAccount {
+    id: string | null;
+    name: string | null;
+    isDefault: boolean | null;
+    kind: string | null;
+    properties: GaProperty[] | [];
+}
+
+/**
+ * GaProperty represents the shape of a Google Analytics property.
+ */
+export interface GaProperty {
+    name: string | null;
+    propertyId: string | null;
+    googleAccountId: string | null;
+    isDefault: boolean | null;
+}
--- src/api/services/analytics/AnalyticsService.ts
+++ src/api/services/analytics/AnalyticsService.ts
@@ -34,6 +34,78 @@ class AnalyticsService {
             console.error("Error fetching Google Analytics data", err);
         }
     }
+
+    /**
+     * This function fetches the Google Analytics accounts and properties for a user.
+     * 
+     * @param userId The user's id
+     * @returns A collection of Google Analytics accounts and properties
+     */
+    async getUserGaAccounts(userId: number): Promise<any> {
+        const response = await fetch(`${this.API_URL}/users/google-accounts/${userId}`, {
+            method: "GET",
+            credentials: "include",
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch Google Analytics accounts");
+        }
+
+        return await response.json();
+    }
+
+    /**
+     * This function fetches the Google Analytics accounts and properties for a user.
+     * 
+     * @returns A collection of Google Analytics accounts and properties
+     */
+    async getGaAccountsAndProperties(): Promise<any> {
+        try {
+            const response = await fetch(`${this.API_URL}/analytics/accounts`, {
+                method: "GET",
+                credentials: "include",
+            });
+
+            const result = await response.json();
+            if (!response.ok) {
+                throw new Error(result.error);
+            }
+            return result.data;
+        } catch(err) {
+            console.error("Error fetching Google Accounts and Properties", err);
+        }
+    }
+
+    /**
+     * This function saves the selected user Google Analytics accounts and properties.
+     * 
+     * @param selectedAccounts A collection of selected Google Analytics accounts and properties.
+     * @returns A promise that resolves to a success or error message.
+     */
+    async saveGaAccountsAndProperties(selectedAccounts: any): Promise<any> {
+        const payload = {
+            accounts: selectedAccounts,
+        };
+
+        try {
+            const response = await fetch(`${this.API_URL}/analytics/accounts`, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(payload),
+            });
+
+            const result = await response.json();
+            if (!response.ok) {
+                throw new Error(result.error);
+            }
+            return result.message;
+        } catch(err) {
+            console.error("Error saving Google Accounts and Properties", err);
+        }
+    }
 }
 
 export default AnalyticsService;
