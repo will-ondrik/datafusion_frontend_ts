4bdb0e9817db72bb6c7525b92cc9699737fbe76c
Will Ondrik <wondrik@my.bcit.ca>
2024-12-17 06:27:23 -0800

    Added guards for all tiers of user. This ensures that unauthorized users can't access pages of higher user tiers.

---
+++ src/guards/Tier1Route.tsx
@@ -0,0 +1,26 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import Spinner from "../components/animations/spinner/spinner";
+import { ProviderProps } from "../types/props/ProviderProp";
+
+/**
+ * Tier1Route is a route guard that checks if the user is authenticated and has a tier level of 1 or higher.
+ * @param param0 - children
+ * @returns - children if the user is authenticated and has a tier level of 1 or higher, otherwise it will redirect to the dashboard.
+ */
+const Tier1Route: React.FC<ProviderProps> = ({ children }) => {
+    const { isAuthenticated, tier, isLoading } = useAuth();
+
+    // If AuthContext is loading, show a spinner
+    if (isLoading) {
+        return (
+            <div>
+                <Spinner />
+            </div>
+        )
+    }
+
+    return isAuthenticated && tier != null && tier >= 1 ? children : <Navigate to="/dashboard" replace />
+}
+
+export default Tier1Route;
---
+++ src/guards/Tier3Route.tsx
@@ -0,0 +1,26 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import Spinner from "../components/animations/spinner/spinner";
+import { ProviderProps } from "../types/props/ProviderProp";
+
+/**
+ * Tier3Route is a route guard that checks if the user is authenticated and has a tier level of 3.
+ * @param param0 - children
+ * @returns - children if the user is authenticated and has a tier level of 3, otherwise it will redirect to the dashboard.
+ */
+const Tier3Route: React.FC<ProviderProps> = ({ children }) => {
+    const { isAuthenticated, tier, isLoading } = useAuth();
+
+    // If AuthContext is loading, show a spinner
+    if (isLoading) {
+        return (
+            <div>
+                <Spinner />
+            </div>
+        )
+    }
+
+    return isAuthenticated && tier != null && tier >= 1 ? children : <Navigate to="/dashboard" replace />
+}
+
+export default Tier3Route;
