b7a3901b12112a84ab67130d8558639bc5e4be5c
Will Ondrik <wondrik@my.bcit.ca>
2024-12-16 18:23:39 -0800

    Removed Strict Mode and wrapped app in the AuthProvider context.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from './context/AuthContex.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ // <StrictMode>
+  <AuthProvider>
     <App />
-  </StrictMode>,
+  </AuthProvider>
+  //</StrictMode>,
 )
