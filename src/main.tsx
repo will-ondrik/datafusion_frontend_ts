06683244dd36dcf2fe1acec7e043c633ef97c857
Will Ondrik <wondrik@my.bcit.ca>
2024-12-18 07:53:08 -0800

    Updated imports.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AuthProvider } from './context/AuthContext.tsx'
+import { AuthProvider } from './context/auth_context.tsx'
 
 createRoot(document.getElementById('root')!).render(
  // <StrictMode>
