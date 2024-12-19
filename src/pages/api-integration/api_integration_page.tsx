aac101b12349b2975567cc2d9a9b67a41029428e
Will Ondrik <wondrik@my.bcit.ca>
2024-12-18 16:40:25 -0800

    Api integration page - users can select their GA accounts/properties.

---
+++ src/pages/api-integration/api_integration_page.tsx
@@ -0,0 +1,13 @@
+import GaAccountSetupModal from "../../components/modals/ga_account_setup";
+
+function ApiIntegrationPage() {
+    return (
+        <div>
+            <h2>API Integration</h2>
+          
+            <GaAccountSetupModal />
+        </div>
+    )
+}
+
+export default ApiIntegrationPage;
