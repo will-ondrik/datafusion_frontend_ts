7fbb62d5ec2e92c34822c9a2437ab41f6357c6c1
Will Ondrik <wondrik@my.bcit.ca>
2024-12-18 16:48:35 -0800

    Props file.

---
+++ src/types/props/Props.ts
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import { GaReportsResponse } from '../../api/dtos/analytics_dtos';
+
+/**
+ * Define the shape of the ProviderProps
+ * 
+ * This interface is used for contexts that provide a value to their children.
+ */
+export interface ProviderProps {
+    children: ReactNode;
+}
+
+/**
+ * Define the shape of the GaDataProps
+ * 
+ * This interface is used for components that receive Google Analytics data.
+ */
+export interface GaDataProps {
+    data: GaReportsResponse;
+}
