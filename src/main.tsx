8e3d1d9ef55260dfde32418d23f61bc1fd58ea42
Will Ondrik <wondrik@my.bcit.ca>
2024-12-19 07:49:43 -0800

    Wrapped App component with new contexts.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/auth_context.tsx'
+import { GaAccountsProvider } from './context/ga_accounts_context.tsx'
+import { GaDataProvider } from './context/ga_data_context.tsx'
 
 createRoot(document.getElementById('root')!).render(
  // <StrictMode>
   <AuthProvider>
-    <App />
+    <GaAccountsProvider>
+      <GaDataProvider>
+        <App />
+      </GaDataProvider>
+    </GaAccountsProvider>
   </AuthProvider>
   //</StrictMode>,
 )
