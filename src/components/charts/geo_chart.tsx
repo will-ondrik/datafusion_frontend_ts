27d698dd246426a29aa83d692ab301cd3a51c06e
Will Ondrik <wondrik@my.bcit.ca>
2025-01-01 22:58:14 -0800

    GeoChart displays country data.

---
+++ src/components/charts/geo_chart.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+import { GeoMap } from "../../api/dtos/analytics_dtos";
+import { Legend } from "chart.js";
+
+export const GeoChart: React.FC<GeoMap>= ({ data }) => {
+  console.log("GeoChart data:", data);
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const geoData = [
+    ["Country", "Sessions"],
+    ...Object.values(data).map(({ country, sessions}) => [country, sessions])
+  ]
+  console.log("GeoData:", geoData);
+
+
+  const options = {
+    backgroundColor: "#242424", 
+    datalessRegionColor: "#333333", // Color for regions without data
+    defaultColor: "#666666", // Default region color
+    colorAxis: { colors: ["#e7f2fa", "#fdc57b", "#ff6f00"] }, // Data gradient
+    legend: "none", 
+  };
+
+  return (
+    <div>
+      <Chart
+        chartType="GeoChart"
+        data={geoData}
+        options={options}
+        width="100%"
+        height="250px"
+      />
+    </div>
+  );
+};
+
+export default GeoChart;
