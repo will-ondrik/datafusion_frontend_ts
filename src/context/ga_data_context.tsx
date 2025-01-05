83426d3f80fcfbb75866d34839daf01beaeb7eb9
Will Ondrik <wondrik@my.bcit.ca>
2025-01-05 12:20:51 -0800

    Fixed caching-related bug. The cache was being overwritten continuously.

--- src/context/ga_data_context.tsx
+++ src/context/ga_data_context.tsx
@@ -2,7 +2,8 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import { useGaAccounts } from './ga_accounts_context';
 import { ProviderProps } from '../types/props/Props';
 import AnalyticsService from '../api/services/analytics_service';
-import { GaReportsResponse, GaRequest, GaTimePeriod } from '../api/dtos/analytics_dtos';
+import { GaReportsResponse, GaRequest, GaTimePeriod, FormattedGaData, GaReport, CardsMetrics, TableMap, GeoMap } from '../api/dtos/analytics_dtos';
+import { formatCardMetrics, formatGeoMetrics, formatTableMetrics } from '../utils/utils';
 
 /** Context type definition */
 interface GaDataContextType {
@@ -20,13 +21,7 @@ interface GaDataContextType {
 /** Default values for the GaDataContext */
 const GaDataContext = createContext<GaDataContextType | null>(null);
 
-/**
- * GaDataProvider
- * @param param0 
- * @returns 
- */
 export const GaDataProvider: React.FC<ProviderProps> = ({ children }) => {
-    // State variables
     const [tabData, setTabData] = useState<Record<string, any>>({});
     const [activeTab, setActiveTab] = useState<string | null>('overview');
     const [startPeriod, setStartPeriod] = useState<GaTimePeriod | null>(null);
@@ -34,22 +29,16 @@ export const GaDataProvider: React.FC<ProviderProps> = ({ children }) => {
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [error, setError] = useState<unknown>(null);
 
-    // External service and context
     const analyticsService = new AnalyticsService();
-    const { defaultAccount, defaultProperty, isLoading: accountsLoading } = useGaAccounts();
+    const { defaultProperty, isLoading: accountsLoading } = useGaAccounts();
+
+    const formatDate = (date: Date): string => date.toISOString().split('T')[0];
 
     /**
-     * Formats a date object to "YYYY-MM-DD" format.
-     * @param date A Date object
-     * @returns A formatted date string
-     */
-    const formatDate = (date: Date): string => {
-        return date.toISOString().split('T')[0];
-    };
-
-    /**
-     * Generates the default start and end dates (last 30 days).
-     * @returns A GaTimePeriod object with start and end dates
+     * Generates a date range used as a default to fetch Google Analytics data.
+     * Users will see data from this date range until choosing a new date range via menu.
+     * 
+     * @returns GaTimePeriod Object
      */
     const generateDefaultStartPeriod = (): GaTimePeriod => {
         const endDate = new Date();
@@ -62,25 +51,57 @@ export const GaDataProvider: React.FC<ProviderProps> = ({ children }) => {
     };
 
     /**
-     * Generates a comparison period based on the given start period.
-     * @param startPeriod The current start period
-     * @returns A GaTimePeriod object representing the comparison period
+     * Generates a date range used to fetch Google Analytics data.
+     * The returned GaTimePeriod contains the date range used to fetch comparison period data.
+     * 
+     * @param startPeriod 
+     * @returns GaTimePeriod Object
      */
     const generateComparisonPeriod = (startPeriod: GaTimePeriod): GaTimePeriod => {
         const duration = new Date(startPeriod.endDate).getTime() - new Date(startPeriod.startDate).getTime();
-        const comparisonStartDate = new Date(new Date(startPeriod.startDate).getTime() - duration);
-        const comparisonEndDate = new Date(new Date(startPeriod.startDate).getTime() - 1);
+        const startDate = new Date(new Date(startPeriod.startDate).getTime() - duration);
+        const endDate = new Date(new Date(startPeriod.startDate).getTime() - 1);
+
         return {
-            startDate: formatDate(comparisonStartDate),
-            endDate: formatDate(comparisonEndDate),
+            startDate: formatDate(startDate),
+            endDate: formatDate(endDate),
         };
     };
 
     /**
-     * Fetches Google Analytics data for a specific tab.
-     * @param tabName The name of the active tab
-     * @param startPeriod The start period for the data
-     * @param comparisonPeriod The comparison period
+     * Conducts formatting of Google Analytics data to correspond with various data visualization components.
+     * 
+     * @param batchReports - Google Analytics API Batch Reports
+     * @returns FormattedGaData - formatted data for data visualization components.
+     */
+    const formatDataForDisplay = (batchReports: GaReportsResponse): FormattedGaData => {
+        const currPeriod: GaReport = {
+            metricTotals: batchReports.currPeriod?.metricTotals || {},
+            reports: batchReports.currPeriod?.reports || [],
+        };
+
+        const compPeriod: GaReport = {
+            metricTotals: batchReports.compPeriod?.metricTotals || {},
+            reports: batchReports.compPeriod?.reports || [],
+        };
+
+        const rawCardsData = [currPeriod.reports[0], compPeriod.reports[0]];
+        const rawTableData = [currPeriod.reports[1]];
+        const rawGeoData = [currPeriod.reports[2]];
+
+        const cardsData: CardsMetrics = formatCardMetrics(rawCardsData);
+        const tableData: TableMap = formatTableMetrics(rawTableData);
+        const geoData: GeoMap = formatGeoMetrics(rawGeoData);
+
+        return { cardsData, tableData, geoData };
+    };
+
+    /**
+     * Fetches Google Analytics data based on the selected Dashboard tab.
+     * 
+     * @param tabName - Tab from the Dashboard page.
+     * @param startPeriod - Date range for the current period's data.
+     * @param comparisonPeriod - Date range for the comparison period's data (used to provide insights on increases/decreases in metric values.).
      */
     const fetchGaDataForTab = async (
         tabName: string,
@@ -97,18 +118,34 @@ export const GaDataProvider: React.FC<ProviderProps> = ({ children }) => {
                 tabName,
                 startPeriod,
                 comparisonPeriod,
-                timeframe: 'date', // static - need to make it dynamic
+                timeframe: 'date',
             };
 
-            const result = await analyticsService.getDashboardData(payload);
-            setTabData((prevData) => ({...prevData, [tabName]: {
-                    ...prevData[tabName], [`${startPeriod.startDate}-${startPeriod.endDate}`]: {
-                        data: [result],
-                        startPeriod,
-                        comparisonPeriod,
+            // Fetch Google Analytics data
+            const result: Error | GaReportsResponse = await analyticsService.getDashboardData(payload);
+            if (result instanceof Error) {
+                throw result;
+            }
+
+            // Format Google Analytics data
+            const { cardsData, tableData, geoData } = formatDataForDisplay(result);
+
+            // Cache Google Analytics data
+            setTabData((prevData) => {
+                const updatedData = {
+                    ...prevData,
+                    [tabName]: {
+                        ...prevData[tabName],
+                        [`${startPeriod.startDate}-${startPeriod.endDate}`]: {
+                            data: { cardsData, tableData, geoData },
+                            startPeriod,
+                            comparisonPeriod,
+                        },
                     },
-                },
-            }));
+                };
+                console.log('Updated tabData:', updatedData);
+                return updatedData;
+            });
         } catch (err) {
             console.error('Failed to fetch GA data:', err);
             setError(err);
@@ -117,22 +154,30 @@ export const GaDataProvider: React.FC<ProviderProps> = ({ children }) => {
         }
     };
 
-     /** Fetch data for the active tab when dependencies change */
     useEffect(() => {
-        console.log('start period', startPeriod);
-        if (accountsLoading || !defaultProperty || !activeTab || !startPeriod || !comparisonPeriod) {
-            console.log(`accountsLoading: ${accountsLoading}, activeTab: ${activeTab}, startPeriod: ${startPeriod}, comparisonPeriod: ${comparisonPeriod}`);
-            return;
-        }
+        console.log('tabData updated:', tabData);
+    }, [tabData]);
+
+    /**
+     * Use Effect checks for required data before:
+     * 
+     *  1) Checking for existing Google Analytics data in the cache.
+     *  2) Fetching Google Analytics data if tab data doesn't exist.
+     */
+    useEffect(() => {
+        if (accountsLoading || !defaultProperty || !activeTab || !startPeriod || !comparisonPeriod) return;
 
         const tabCache = tabData[activeTab]?.[`${startPeriod.startDate}-${startPeriod.endDate}`];
-
         if (!tabCache) {
             fetchGaDataForTab(activeTab, startPeriod, comparisonPeriod);
         }
     }, [activeTab, startPeriod, comparisonPeriod, accountsLoading, defaultProperty]);
 
-    /** Initialize start and comparison periods when the provider mounts */
+    /**
+     * Use Effect generates and sets date ranges for current/comparison period date ranges.
+     * 
+     * TODO: Only generate a default start period if the user hasn't selected one.
+     */
     useEffect(() => {
         const defaultStartPeriod = generateDefaultStartPeriod();
         const defaultComparisonPeriod = generateComparisonPeriod(defaultStartPeriod);
@@ -140,8 +185,6 @@ export const GaDataProvider: React.FC<ProviderProps> = ({ children }) => {
         setComparisonPeriod(defaultComparisonPeriod);
     }, []);
 
-   
-    /** Provide the state and functions through the context */
     return (
         <GaDataContext.Provider
             value={{
@@ -161,10 +204,6 @@ export const GaDataProvider: React.FC<ProviderProps> = ({ children }) => {
     );
 };
 
-/**
- * Custom hook to consume the GaDataContext.
- * @returns The GaDataContext value
- */
 export const useGaData = (): GaDataContextType => {
     const context = useContext(GaDataContext);
     if (!context) {
